9011f0005066337d91acc97e3ec9925dca0f2628
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-04-20 09:56:38 +0700

    video18.2

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,31 @@ const Layout = () => {
   )
 }
 
+const LayoutAdmin = () => {
+  const isAdminRoute = window.location.pathname.startsWith('/admin');
+  const user = useSelector(state => state.account.user);
+  const userRole = user.role;
+
+  return (
+    <div className='layout-app'>
+      {isAdminRoute && userRole === "ADMIN" && <Header />}
+      <Outlet />
+      {isAdminRoute && userRole === "ADMIN" && <Footer />}
+    </div>
+  )
+}
+
 
 export default function App() {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.account.isAuthenticated);
 
   const getAccount = async () => {
-    if (window.location.pathname === '/login') return;
+    if (
+      window.location.pathname === '/login'
+      || window.location.pathname === '/register'
+      || window.location.pathname === '/'
+    ) return;
     const res = await callFetchAccount();
     if (res && res.data) {
       dispatch(doGetAccountAction(res.data));
@@ -66,7 +84,7 @@ export default function App() {
     },
     {
       path: "/admin",
-      element: <Layout />,
+      element: <LayoutAdmin />,
       errorElement: <NotFound />,
 
       children: [
@@ -98,7 +116,10 @@ export default function App() {
   return (
     <>
       {isAuthenticated === true
-        || window.location.pathname === '/admin' ?
+        || window.location.pathname === '/login'
+        || window.location.pathname === '/register'
+        || window.location.pathname === '/'
+        ?
         <RouterProvider router={router} />
         :
         <Loading />
--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,5 +1,18 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import NotPermitted from "./NotPermitted";
+
+const RoleBaseRoute = (props) => {
+    const isAdminRoute = window.location.pathname.startsWith('/admin');
+    const user = useSelector(state => state.account.user);
+    const userRole = user.role;
+
+    if (isAdminRoute && userRole === "ADMIN") {
+        return (<>{props.children}</>)
+    }
+    return <NotPermitted />
+
+}
 
 const ProtectedRoute = (props) => {
     const isAuthenticated = useSelector(state => state.account.isAuthenticated);
@@ -9,7 +22,7 @@ const ProtectedRoute = (props) => {
                 ?
                 <>{props.children}</>
                 :
-                <Navigate to='/login' replace />
+                <Navigate to='/login' replace={true} />
             }
         </>
     )
