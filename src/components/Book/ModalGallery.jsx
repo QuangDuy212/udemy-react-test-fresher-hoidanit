1dcb5db8ddb791a405909f28466f8b5e7b7c209d
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-05-06 22:26:38 +0700

    video55

--- src/components/Book/ModalGallery.jsx
+++ src/components/Book/ModalGallery.jsx
@@ -1,12 +1,21 @@
 
 import { Button, Col, Modal, Row, Image } from 'antd';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ImageGallery from "react-image-gallery";
+import './ModalGallery.scss';
+
+
 const ModalGallery = (props) => {
-    const { isOpenModalGallery, setIsOpenModalGallery, images } = props;
+    const { isOpenModalGallery, setIsOpenModalGallery, images, currentIndex } = props;
     const [dataPreview, setDataPreview] = useState(images[0].original);
+    const refGallery = useRef();
+
     const handleCancel = () => {
         setIsOpenModalGallery(false);
+    }
+
+    const handleOnClickThumnail = (index) => {
+        refGallery.current.slideToIndex(index)
     }
 
     return (
@@ -14,16 +23,46 @@ const ModalGallery = (props) => {
             <Modal
                 open={isOpenModalGallery}
                 onCancel={handleCancel}
-                width={"850px"}
+                width={"1000px"}
                 closable={false}
                 footer={<></>}
             >
-                <ImageGallery
-                    items={images}
-                    thumbnailPosition='right'
-                    showPlayButton={false}
-                    showFullscreenButton={false}
-                />
+                <Row gutter={[15, 15]} style={{ overflow: "hidden" }}>
+                    <Col xl={16} md={24}>
+                        <div className='image'>
+                            <ImageGallery
+                                items={images}
+                                ref={refGallery}
+                                thumbnailPosition='right'
+                                showPlayButton={false}
+                                showFullscreenButton={false}
+                                showThumbnails={false}
+                                startIndex={currentIndex}
+                                width={100}
+                            />
+                        </div>
+                    </Col>
+                    <Col xl={8} md={24}>
+                        <Row gutter={[15, 15]}>
+                            {images?.map((item, index) => {
+                                return (
+                                    <Col xl={8} md={24} key={`image-${index}`}>
+                                        <div className='thumbnail'
+                                        >
+                                            <Image
+                                                src={item.thumbnail}
+                                                width={100}
+                                                height={100}
+                                                preview={false}
+                                                onClick={() => { handleOnClickThumnail(index) }}
+                                            />
+                                        </div>
+                                    </Col>
+                                )
+                            })}
+                        </Row>
+                    </Col>
+                </Row>
             </Modal>
         </>
     )
--- src/components/Book/ViewDetail.jsx
+++ src/components/Book/ViewDetail.jsx
@@ -1,11 +1,52 @@
 import { Button, Col, Row, Rate, InputNumber } from 'antd';
 import './book.scss';
 import ImageGallery from "react-image-gallery";
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FaPlus, FaMinus } from "react-icons/fa6";
 import ModalGallery from './ModalGallery';
+import logoShip from '../../../public/logo/logoShip.png';
+import { FaCartPlus } from "react-icons/fa";
 
 const images = [
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+        thumbnailTitle: "vip"
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+        thumbnailTitle: "vip"
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+        thumbnailTitle: "vip"
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
     {
         original: "https://picsum.photos/id/1018/1000/600/",
         thumbnail: "https://picsum.photos/id/1018/250/150/",
@@ -27,6 +68,14 @@ const onChange = (value) => {
 
 const ViewDetail = (props) => {
     const [isOpenModalGallery, setIsOpenModalGallery] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const refGallery = useRef(null);
+
+    const handleOnClickImage = () => {
+        setIsOpenModalGallery(true);
+        setCurrentIndex(refGallery?.current?.getCurrentIndex() ?? 0);
+    }
+
     return (
         <>
             <div className='container'>
@@ -34,35 +83,43 @@ const ViewDetail = (props) => {
                     <Row gutter={[20, 20]}>
                         <Col xl={10} md={24}>
                             <ImageGallery
+                                ref={refGallery}
                                 items={images}
                                 showFullscreenButton={false}
                                 showNav={false}
                                 showPlayButton={false}
                                 slideOnThumbnailOver={true}
-                                onClick={() => setIsOpenModalGallery(true)}
+                                onClick={() => handleOnClickImage()}
                             />
                         </Col>
                         <Col xl={14} md={24}>
                             <div className='viewdetail__info'>
-                                <div className='viewdetail__info--author'>Tác giả: Quang Duy</div>
                                 <div className='viewdetail__info--name'>Đắc nhân tâm</div>
+                                <div className='viewdetail__info--author'>Tác giả: Quang Duy</div>
                                 <div className='viewdetail__info--rate'><Rate disabled defaultValue={5} /> Đã bán 30</div>
                                 <div className='viewdetail__info--price'>
                                     {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(100000)}
                                 </div>
-                                <div className='viewdetail__info--transport'>Vận chuyển:
-                                    <span>Miễn phí vận chuyển</span>
+                                <div className='viewdetail__info--transport'>
+                                    <span className='text1'>Vận chuyển </span>
+                                    <span className='text2'>
+
+                                        <img src={logoShip} style={{ height: 20 }} />
+                                        <span>Miễn phí vận chuyển</span>
+                                    </span>
                                 </div>
                                 <div className='viewdetail__info--quantity'>
-                                    Số lượng:
+                                    <span className='text1'>Số lượng</span>
+
                                     <InputNumber min={1} max={10} defaultValue={3} onChange={onChange}
-                                        addonAfter={<FaPlus />}
-                                        addonBefore={<FaMinus />}
                                     />
                                 </div>
                                 <div className='viewdetail__info--btn'>
-                                    <Button>Thêm vào giỏ hàng</Button>
-                                    <Button>Mua ngay</Button>
+                                    <Button type='button' className='add'>
+                                        <FaCartPlus style={{ margin: "0 5px 0 0", fontSize: "20px" }} />
+                                        Thêm vào giỏ hàng
+                                    </Button>
+                                    <Button className='buy'>Mua ngay</Button>
                                 </div>
 
                             </div>
@@ -73,6 +130,7 @@ const ViewDetail = (props) => {
             <ModalGallery
                 isOpenModalGallery={isOpenModalGallery}
                 setIsOpenModalGallery={setIsOpenModalGallery}
+                currentIndex={currentIndex}
                 images={images}
             />
         </>
